cfc772ed6c3e1a1a2054e0029d1d266c559fa825
Lotus <egoo.geovany@gmail.com>
2021-05-11 11:32:40 -0600

    First Commit and Login Page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './shared/login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ImportModule } from './import.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, LoginComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ImportModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
